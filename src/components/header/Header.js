73f1076bda427faec69a590c60b0f154ac8a7e7f
MartinNikolov <martin.v.nikolov93@gmail.com>
2020-07-29 23:05:08 +0300

    Changed navigation links to NavLink and some css changes

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,19 +1,19 @@
 import React from 'react'
 
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 import styles from './Header.module.css'
 
 const Header = () => {
     return (
         <nav className={styles.navigation}>
-            <span className={styles.logo}>Recipes</span>
+            <h2 className={styles.logo}>React Recipes</h2>
             <div className={styles['nav-buttons-wrapper']}>
-                <Link className={styles['nav-button']} to='/'>Home</Link>
-                <Link className={styles['nav-button']} to='/favourites'>Favourites</Link>
-                <Link className={styles['nav-button']} to='/profile'>Profile</Link>
-                <Link className={styles['nav-button']} to='/login'>Login</Link>
-                <Link className={styles['nav-button']} to='/register'>Register</Link>
+                <NavLink className={styles['nav-button']} to='/' exact activeClassName={styles['nav-button-active']}>Home</NavLink>
+                <NavLink className={styles['nav-button']} to='/favourites' activeClassName={styles['nav-button-active']}>Favourites</NavLink>
+                <NavLink className={styles['nav-button']} to='/profile' activeClassName={styles['nav-button-active']}>Profile</NavLink>
+                <NavLink className={styles['nav-button']} to='/login' activeClassName={styles['nav-button-active']}>Login</NavLink>
+                <NavLink className={styles['nav-button']} to='/register' activeClassName={styles['nav-button-active']}>Register</NavLink>
             </div>
         </nav>
     )
